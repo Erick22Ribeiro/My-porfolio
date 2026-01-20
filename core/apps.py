168cca8b48cebd5b4235dda3c10257819e500fa9
Erick Ribeiro <erick2ribeirogg@gmail.com>
2026-01-20 11:14:08 -0300

    CriaçÃo do superUser pelo render

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,20 @@
 from django.apps import AppConfig
-
+import os
 
 class CoreConfig(AppConfig):
-    name = 'core'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'  # o nome do seu app
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        # pega as variáveis de ambiente
+        username = os.environ.get("DJANGO_ADMIN_USER")
+        email = os.environ.get("DJANGO_ADMIN_EMAIL")
+        password = os.environ.get("DJANGO_ADMIN_PASSWORD")
+
+        # cria superuser se não existir
+        if username and email and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username=username, email=email, password=password)
