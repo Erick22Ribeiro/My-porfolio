ec906d3fc3ea001a9cd7bfd8c27d020b20414f3d
Erick Ribeiro <erick2ribeirogg@gmail.com>
2026-01-20 11:21:35 -0300

    Criação super user 02

--- core/apps.py
+++ core/apps.py
@@ -1,20 +1,23 @@
 from django.apps import AppConfig
 import os
+from django.db.utils import OperationalError
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'  # o nome do seu app
+    name = 'core'
 
     def ready(self):
         from django.contrib.auth import get_user_model
         User = get_user_model()
+        
+        try:
+            username = os.environ.get("DJANGO_ADMIN_USER")
+            email = os.environ.get("DJANGO_ADMIN_EMAIL")
+            password = os.environ.get("DJANGO_ADMIN_PASSWORD")
 
-        # pega as variáveis de ambiente
-        username = os.environ.get("DJANGO_ADMIN_USER")
-        email = os.environ.get("DJANGO_ADMIN_EMAIL")
-        password = os.environ.get("DJANGO_ADMIN_PASSWORD")
-
-        # cria superuser se não existir
-        if username and email and password:
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(username=username, email=email, password=password)
+            if username and email and password:
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(username=username, email=email, password=password)
+        except OperationalError:
+            # banco ainda não está pronto, ignora
+            pass
