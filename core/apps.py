377eb59f64f35a60904b8524adc2079dae3a408d
Erick Ribeiro <erick2ribeirogg@gmail.com>
2026-01-20 11:26:42 -0300

    Criação super user 03

--- core/apps.py
+++ core/apps.py
@@ -1,23 +1,5 @@
 from django.apps import AppConfig
-import os
-from django.db.utils import OperationalError
+
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-        
-        try:
-            username = os.environ.get("DJANGO_ADMIN_USER")
-            email = os.environ.get("DJANGO_ADMIN_EMAIL")
-            password = os.environ.get("DJANGO_ADMIN_PASSWORD")
-
-            if username and email and password:
-                if not User.objects.filter(username=username).exists():
-                    User.objects.create_superuser(username=username, email=email, password=password)
-        except OperationalError:
-            # banco ainda não está pronto, ignora
-            pass
--- core/views.py
+++ core/views.py
@@ -7,70 +7,6 @@ from .forms import ContatoForm
 from django.urls import reverse
 from django.http import JsonResponse #para retornar compativel com o fetch
 
-# Create your views here.
-""" def home(request):
-
-    skills = Skill.objects.all()
-    skills_2 = Skill_2.objects.all()
-    bancos = Banco.objects.all()
-
-    projetos = Projeto.objects.all()
-
-    skills_labels = [skill.nome for skill in skills]    #para o grafico 
-    skills_values = [skill.nivel for skill in skills]
-    skills_colors = [skill.cor for skill in skills]
-
-    #formulario 
-    if request.method == 'POST':
-        
-        form = ContatoForm(request.POST)
-
-        if form.is_valid():
-            
-            contato = form.save() #salva no banco
-
-            messages.success(request, "Mensagem enviada!")
-
-            nome = contato.nome
-            email = contato.email
-            mensagem = contato.mensagem
-
-            #envia email pra mim
-            send_mail(
-                subject=f'Novo contato - {nome}',
-                message=f'''Nova mensagem pelo portfólio: 
-Nome: {nome}
-Email: {email}
-
-Mensagem: {mensagem}''',
-                
-                from_email=None,
-                recipient_list=['erick22reserva@gmail.com'],
-                fail_silently=False,
-            )
-
-            #return redirect('home')
-            return JsonResponse({"status": "ok"}) #Fetch não trabalha bem com redirect/render — ele trabalha com JSON.
-        
-    else:
-        form = ContatoForm()
-
-
-    context = {
-        'skills': skills,   #passa as skills
-        'bancos': bancos,
-        'skills2': skills_2,
-        'projetos': projetos,
-        'form' : form,     
-
-        'skills_labels': json.dumps(skills_labels),     #passa as informacoes pro grafico
-        'skills_values': json.dumps(skills_values), 
-        'skills_colors': json.dumps(skills_colors),
-    }
-
-    return render(request, 'core/home.html', context)
-
- """
 def home(request):
 
     # Detecta se é requisição de traduçÃo
@@ -253,3 +189,19 @@ Mensagem: {mensagem}''',
 
     return render(request, 'core/home.html', context)
 
+
+# views.py
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+import os
+
+def criar_superuser(request):
+    User = get_user_model()
+    username = os.environ.get("DJANGO_ADMIN_USER", "admin")
+    email = os.environ.get("DJANGO_ADMIN_EMAIL", "admin@admin.com")
+    password = os.environ.get("DJANGO_ADMIN_PASSWORD", "admin123")
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        return HttpResponse("Superuser criado!")
+    return HttpResponse("Superuser já existe!")
